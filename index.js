ae67a2b93d743aa00d062c1a6b9dea1f75c8377d
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-21 17:41:32 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+const record=[
+    {year:'2015', result:'W'},
+    { year:'2014', result:'N/A'},
+    {year:'2013', result:'L'}
+]
+function superbowlWin(record){
+    
+    // for(item of element){
+        let resultW= record.find(function (record) {
+            return record.result==='W'
+            
+        })
+        if(resultW){
+            return resultW.year
+        // }
+    }
+    
+}
+console.log(record.find(superbowlWin))
